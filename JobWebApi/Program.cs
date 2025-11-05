538ca09032c8be91b7aeaf4c8af9a51ffde2e24e
RASOLOFOSON fifalina <takolamada301@gmail.com>
2025-11-05 17:07:52 +0300

    Feat Job Web Api

---
+++ JobWebApi/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using JobWebApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobWebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            // R�cupere la chaine de connexion de la base dans les parametres
+            string? connect = builder.Configuration.GetConnectionString("JobWebApiConnect");
+
+            // Enregistre ton DbContext dans le conteneur DI
+            builder.Services.AddDbContext<ContextJobWebApi>(options =>
+                options.UseSqlServer(connect));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
